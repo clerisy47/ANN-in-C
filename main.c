548687b18625d0e361c203764d6b21b8aae97ca7
Shrine Sidgel <123295702+ShrineSigdel@users.noreply.github.com>
2023-07-30 22:12:13 +0545

    Created relu function

--- main.c
+++ main.c
@@ -3,8 +3,31 @@
 #include<math.h>
 #include<stdlib.h>
 
-int main(){
+void reluToEachElement(float* inputArray, int size);
+float relu(float x);
+
+int main() {
+    // Initialize testArray with some values
+    float testArray[] = {32, -32, 21, -23, 21, 32, 23, 232, 1, 32.7};
+    int size = sizeof(testArray) / sizeof(testArray[0]);
+
+
+    // Apply ReLU to each element of the array
+    reluToEachElement(testArray, size);
+
 
 
     return 0;
+}
+
+// relu function to take each element of an array and apply ReLU
+void reluToEachElement(float* inputArray, int size) {
+    for (int i = 0; i < size; i++) {
+        inputArray[i] = relu(inputArray[i]);
+    }
+}
+
+// relu function
+float relu(float x) {
+    return (x > 0) ? x : 0;
 }

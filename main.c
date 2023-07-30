e274f8e84df273626a62bf5ff9721dc880bdb40b
nishanadhikari2061 <adhikarin537@gmail.com>
2023-07-30 23:56:48 +0545

    Created sigmoid function

--- main.c
+++ main.c
@@ -3,8 +3,37 @@
 #include<math.h>
 #include<stdlib.h>
 
-int main(){
+void SigmoidOfElements(float  *input_Array, int no_of_elements);
+float SigmoidArray(float x);
 
+int main() {
+    // Initialize inputArray
+    float input_Array[] = {0.1,0.65,1,0,-0.34,-0.02};
+    int no_of_elements = sizeof(input_Array) / sizeof(input_Array[0]);
 
+    printf("\nInputs for sigmoid function:\n");
+    for (int i = 0; i < no_of_elements; i++) {
+        printf("%f\n",input_Array[i]);
+    }
+    
+
+    // call sigmoid function
+    SigmoidOfElements(input_Array, no_of_elements);
+    printf("\nOutputs after applying sigmoid:\n");
+    for (int i = 0; i < no_of_elements; i++) {
+        printf("%f\n",input_Array[i]);
+    }
     return 0;
+}
+
+// Passing Each element of array to sigmodArray Function.
+void SigmoidOfElements(float  *input_Array, int no_of_elements) {
+    for (int i = 0; i < no_of_elements; i++) {
+        input_Array[i] = SigmoidArray(input_Array[i]);
+    }
+}
+
+// Applying sigmoid function to each element of array
+float SigmoidArray(float x) {
+    return(1/(1+exp(-x)));  
 }

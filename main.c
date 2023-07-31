eabc660eeedf25b73e0c7344c486fe1518eafcc7
nishanadhikari2061 <adhikarin537@gmail.com>
2023-07-31 23:13:35 +0545

    added random number generator

--- main.c
+++ main.c
@@ -2,14 +2,17 @@
 #include<string.h>
 #include<math.h>
 #include<stdlib.h>
+#include<time.h>
 
 void SigmoidOfElements(float  *input_Array, int no_of_elements);
 float SigmoidArray(float x);
 void reluToEachElement(float* inputArray, int size);
 float relu(float x);
+void generateNumbers(float *array, int size);
 
 
 int main() {
+    srand(time(NULL)); // for generating random numbers , must be in main do not remove
     return 0;
 }
 
@@ -35,4 +38,12 @@ void reluToEachElement(float* inputArray, int size) {
 // relu function
 float relu(float x) {
     return (x > 0) ? x : 0;
+}
+
+//random function
+void generateNumbers(float *array, int size) {
+    for (int i = 0; i < size; i++) {
+        array[i] = (float)rand() / RAND_MAX;
+        printf("%f\t",array[i]);
+    }
 }

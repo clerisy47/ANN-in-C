f4266fc7cfffe4670b51004955b6f26d1807c079
Ros0n <roshanrijal4567@gmail.com>
2023-08-15 15:55:12 +0545

    Updated Main.c and created new file

--- main.c
+++ main.c
@@ -37,32 +37,3 @@ void tanhyper(float *inputArray, int size)
     }
 }
 
-//Function to multiply the matrices
-float** Multiply_Matrices(float** Matrix1, int rows1, int cols1, float** Matrix2, int rows2, int cols2){
-
-    //allocating memory for result matrix
-    float** resultMatrix = (float**) malloc(rows1*sizeof(float*));
-    for (int i = 0; i < rows1; i++){
-        resultMatrix[i]= (float*) malloc(cols2*sizeof(float));
-    }
-
-    //Multiplying the matrices
-    for(int i = 0; i < rows1; i++){
-        for(int j = 0; j < cols2; j++){
-            resultMatrix[i][j]=0;
-            for(int k = 0; k < cols1; k++){
-                resultMatrix[i][j] =  resultMatrix[i][j] + (Matrix1[i][k]*Matrix2[k][j]);
-            }
-        }
-    }
-    return resultMatrix;
-}
-   
-
-    // function for freeing the memory
-    void free_memory(float** matrix, int rows){
-        for(int i=0; i<rows; i++){
-            free(matrix[i]);
-    }
-    free(matrix);
-}
